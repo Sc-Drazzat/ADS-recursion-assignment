8ad49f73ac40f148c3300999667699481ccbd89c
Ayan Rakhimov <143061005+Dr-Drazzat@users.noreply.github.com>
2026-03-25 21:47:34 +0500

    Ayan Rakhimov task 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+    //1
     public static int sumOfSquares(int n) {
         //Time complexity is O(n), function call itself n times from n to 1
         //Space complexity is O(n) each recursive call takes place in stack, starting from n to 1
         if (n == 1) return 1;
         return n * n + sumOfSquares(n - 1);
     }
+    //2
     public static int sumOfElements(int[] array, int n){
-        if (n==0) return array[0];
+        //Time complexity is O(n), function call itself n times from element arr[n-1] to arr[0]
+        //Space complexity is O(n) each recursive call takes place in stack, starting element arr[n-1] to arr[0]
+        if (n==1) return array[0];
         return array[n-1] + sumOfElements(array, n-1);
     }
+    //3
+
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8};
